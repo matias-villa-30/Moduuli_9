541fd621721469cf886c2a22bfafdd20d990185d
villa <matias.villalemes@metropolia.fi>
2024-10-11 12:02:57 +0300

    harj 1 tehty

--- auto.py
+++ auto.py
@@ -1,2 +1,14 @@
-class Auto:
-    pass
+class Auto():
+    def __init__(self, rekisteritunnus, huippunopeus, tämänhetkinen_nopeus=0, kuljettu_matka=0):
+        self.rekisteritunnus = rekisteritunnus
+        self.huippunopeus = huippunopeus
+        self.tämänhetkinen_nopeus = tämänhetkinen_nopeus
+        self.kuljettu_matka = kuljettu_matka
+
+    def main(self):
+        return self.rekisteritunnus, self.huippunopeus, self.tämänhetkinen_nopeus, self.kuljettu_matka
+
+
+auto = Auto("ABC123", "142 km/h")
+
+print(auto.main())
