a36f77838cc57b53b5529388428177142ffadb88
villa <matias.villalemes@metropolia.fi>
2024-10-13 20:57:03 +0300

    harj 2 tehty

---
+++ 02_kiihdytä.py
@@ -0,0 +1,31 @@
+class Auto():
+    def __init__(self, rekisteritunnus, huippunopeus, tämänhetkinen_nopeus=0, kuljettu_matka=0):
+        self.rekisteritunnus = rekisteritunnus
+        self.huippunopeus = huippunopeus
+        self.tämänhetkinen_nopeus = tämänhetkinen_nopeus
+        self.kuljettu_matka = kuljettu_matka
+
+    def accelerate(self, tämänhetkinen_nopeus, velocidad):
+
+
+        self.velocidad = velocidad
+        self.tämänhetkinen_nopeus += velocidad
+
+        if self.tämänhetkinen_nopeus > 142:
+            self.tämänhetkinen_nopeus = 140
+
+        elif self.tämänhetkinen_nopeus < 0:
+            self.tämänhetkinen_nopeus = 0
+
+
+
+    def main(self):
+        self.accelerate(self.tämänhetkinen_nopeus, 30)
+        self.accelerate(self.tämänhetkinen_nopeus, 70)
+        self.accelerate(self.tämänhetkinen_nopeus, 50)
+        print(f"Current speed is: {self.tämänhetkinen_nopeus} km/h")
+        self.accelerate(self.tämänhetkinen_nopeus, -200)
+        return self.rekisteritunnus, self.huippunopeus, self.tämänhetkinen_nopeus, self.kuljettu_matka
+
+auto = Auto("ABC123", "142 km/h")
+print(auto.main())
